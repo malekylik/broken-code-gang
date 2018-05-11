4a9e905cfe91b117311a5c782af27489c6a4b014
Maxim Kalinovskiy <malekylik@gmail.com>
2018-05-11 19:33:52 +0300

    sort chat without messegae at the top

--- src/helpers/compareMessages.js
+++ src/helpers/compareMessages.js
@@ -1,6 +1,6 @@
 export function compareMessages(a, b) {
-    a = (a && a.lastMessage && a.lastMessage.created_at) || 0;
-    b = (b && b.lastMessage && b.lastMessage.created_at) || 0;
+    a = (a && a.lastMessage && a.lastMessage.created_at) || Number.MAX_SAFE_INTEGER;
+    b = (b && b.lastMessage && b.lastMessage.created_at) || Number.MAX_SAFE_INTEGER;
 
     return b - a;
 }
